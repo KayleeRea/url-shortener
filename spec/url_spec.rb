d3679ece36c39d810c955cc72dcdcf76c7a4d0ad
Gus King <gusssk@gmail.com>
2014-03-28 17:14:11 -0600

    [#68381170] We refactored form input in our Capybara test URL to solve the infinite redirect loop.
    Signed-off-by: Mike Kauffman <mike.a.kauffman@gmail.com>

--- spec/url_spec.rb
+++ spec/url_spec.rb
@@ -8,9 +8,12 @@ feature 'Manages Homepage' do
 
   scenario 'user visits the homepage' do
     visit '/'
-    fill_in "Type URL to shorten here", :with => 'http://tutorials.gschool.it'
+    fill_in "Type URL to shorten here", :with => 'tutorials.gschool.it'
     click_button 'Shorten'
-    expect(page).to have_content "http://tutorials.gschool.it"
+    expect(page).to have_content "tutorials.gschool.it"
     expect(page).to have_content "http://www.example.com/1"
+
+    visit "/1"
+    current_url.should == "http://tutorials.gschool.it/"
   end
 end
